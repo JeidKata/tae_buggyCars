f2321f7b614a7f98b890f4200f1ea37df28bcdab
JeidyOlaya <jeidkata@gmail.com>
2025-12-11 14:38:06 -0500

    feat: Agrego los datos con los que voy a realizar las pruebas que van a mandar mensaje de error.

--- src/test/java/com/aviatur/buggy_car/data/Data.java
+++ src/test/java/com/aviatur/buggy_car/data/Data.java
@@ -1,5 +1,18 @@
 package com.aviatur.buggy_car.data;
 
+import org.testng.annotations.DataProvider;
+
 public class Data {
 
+    @DataProvider(name = "registerData")
+    public Object [] [] registerData(){
+        return new Object[][]{
+                {"", "", "", "", "", "Campos requeridos."},
+                {"TestPrueba", "Test", "Prueba", "123t", "123t","Password corta"},
+                {"TestPrueba", "Test", "Prueba", "123t#", "123t#","Password minimo 6"},
+                {"TestPrueba", "Test", "Prueba", "123test654#", "123test654#","No cumple condiciones"},
+                {"TestPrueba", "Test", "Prueba", "123tesT654#", "123testT654#","No coinciden"},
+                {"TestPrueba", "Test", "Prueba", "123tesT654#", "123tesT654#","Ya existe usuario"},
+        };
+    }
 }
