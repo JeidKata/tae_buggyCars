340693ebaf28ea59a531cabe3316b3de865108d9
JeidyOlaya <jeidkata@gmail.com>
2025-12-11 12:24:39 -0500

    feat: Agrego el locator y función de la pagina principal para iniciar el registro de un nuevo usuario.

--- src/test/java/com/aviatur/buggy_car/pages/MainPage.java
+++ src/test/java/com/aviatur/buggy_car/pages/MainPage.java
@@ -1,5 +1,19 @@
 package com.aviatur.buggy_car.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class MainPage extends BasePage {
 
+    @FindBy(className = "btn-success-outline")
+    private WebElement btnRegister;
+
+    public MainPage(WebDriver driver){
+        super(driver);
+    }
+
+    public void clicOnRegister(){
+        clicOn(btnRegister);
+    }
 }
